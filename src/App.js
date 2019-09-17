07e6615389752005ab1866c7a4670fa5e7e93369
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-17 13:49:06 +0900

    learn about props and defaultProps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 const App = () => {
+  const details = [
+    {name: "Meow", age: 10},
+    {name: "Hanada", age: 20},
+    {}
+  ]
   return (
     <div>
-    <Cat />
-    <Cat />
+      {
+        details.map((detail, index) => {
+          return <Cat name={detail.name} age={detail.age} key={index}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const Cat = (props) => {
+  return <div>My name is {props.name}.My age is {props.age}</div>
+}
+
+Cat.defaultProps = {
+  name: "Nanashi",
+  age: 0
 }
 
 export default App;
